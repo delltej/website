d331e628c97e1fb0750964a6bc02cf9939ecbfbb
dellt1 <tsinghmathoda@gmail.com>
2024-07-03 08:22:23 -0400

    v1

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const activeSection = document.getElementById(sectionId);
+    activeSection.style.display = 'block';
+}
+
+// Initially display the home section
+document.getElementById('home').style.display = 'block';
